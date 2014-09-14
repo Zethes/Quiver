31456c45716216f22712ded686e5da9d884471c2
Amber Thrall <amber.rose.thrall@gmail.com>
2014-09-14 02:21:39 -0700

    Created world and chunks.

--- src/world/tile.d
+++ src/world/tile.d
@@ -2,27 +2,28 @@ module world.tile;
 
 enum TileIds
 {
-	WALL
+    WALKABLE,
+    WALL
 };
 
 class Tile
 {
-	this(TileIds id, int x, int y) 
-	{
-		this.id = id;
-		this.x = x;
-		this.y = y;
-	}
+    this(TileIds id, int x, int y) 
+    {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
 
-	char GetSymbol() 
-	{
-		return '?';
-	}
+    abstract char GetSymbol() 
+    {
+        return ' ';
+    }
 
-	@property TileIds Id() { return id; }
-	@property int X() { return x; }
-	@property int Y() { return y; }
+    @property TileIds Id() { return id; }
+    @property int X() { return x; }
+    @property int Y() { return y; }
 private:
-	TileIds id;
-	int x, y;
+    TileIds id;
+    int x, y;
 };
---
+++ src/world/tiles/walkable.d
@@ -0,0 +1,15 @@
+module world.tiles.walkable;
+import world.tile;
+
+class Walkable : Tile
+{
+	this(int x, int y)
+	{
+		super(TileIds.WALKABLE, x, y);
+	}
+
+	override char GetSymbol() 
+	{
+		return ' ';
+	}
+}
