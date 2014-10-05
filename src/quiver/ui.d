e5a4fb0f0aad0e91dda98367217844aa185c8621
Amber Thrall <amber.rose.thrall@gmail.com>
2014-10-04 17:35:24 -0700

    Updated gitignore

---
+++ src/quiver/ui.d
@@ -0,0 +1,114 @@
+module quiver.ui;
+import render.window;
+import render.screen;
+import render.canvas;
+import util.vector;
+import util.log;
+
+class UI
+{
+    static register(string name, Window window)
+    {
+        windows ~= window;
+        windowNames ~= name;
+    }
+
+    static Window getWindow(string name)
+    {
+        foreach (i, win; windows)
+        {
+            if (name == windowNames[i])
+            {
+                return win;
+            }
+        }
+
+        return null;
+    }
+
+    static draw()
+    {
+        foreach (i, win; windows)
+        {
+            win.clear();
+            win.addBorder();
+            win.print(VectorI(1,0), windowNames[i]);
+            win.draw();
+            win.refresh();
+        }
+    }
+
+private:
+    static Window[] windows;
+    static string[] windowNames;
+};
+
+enum ChatMessageTypes
+{
+    clientMessage,
+};
+
+class ChatWindow : Window
+{
+    this(VectorI screenSize, int maxMsgs = 1024)
+    {
+        this.maxMsgs = maxMsgs;
+        resize(screenSize);
+        super(pos, size);
+    }
+
+    void resize(VectorI screenSize)
+    {
+        //TODO dynamic this shit.
+        size.x = screenSize.x;
+        size.y = 7;
+        pos.x = 0;
+        pos.y = screenSize.y - size.y;
+    }
+    
+    void addMessage(ChatMessageTypes type, string msg)
+    {
+        messages ~= msg;
+        types ~= type;
+
+        if (messages.length > maxMsgs) {
+            
+        }
+    }
+
+    override void draw()
+    {
+        auto msgPos = VectorI(1,1);
+        
+        //TODO: last X messages.
+        for (auto i = 0; i < messages.length; ++i)
+        {
+            string msg = messages[i];
+
+            byte color = WHITE_ON_BLACK;
+            switch (types[i])
+            {
+                case ChatMessageTypes.clientMessage: 
+                    color = YELLOW_ON_BLACK;
+                    break;
+                default:
+                    color = WHITE_ON_BLACK;
+                    break;
+            }
+
+            print(msgPos, msg, color);
+            msgPos.y++;
+
+            if (msg.length >= size.x-2)
+                msgPos.y++;
+
+            if (msgPos.y >= size.y - 1)
+                break;
+        }
+    }
+private:
+    VectorI size, pos;
+    string[] messages;
+    int maxMsgs;
+    ChatMessageTypes[] types;
+};
